4cf592fbf95c46670ef2ca191391b6e2dcf801e8
virolivs <103077000+virolivs@users.noreply.github.com>
2022-05-25 23:03:36 -0300

    Added routes to QuoteController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import QuoteController from '@controllers/QuoteController'
 
 const routes = express.Router();
 const userController = new UserController();
+const userController = new QuoteController();
 
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
+
+routes.post('/quote', QuoteController.create);
 
 
 export default routes;
