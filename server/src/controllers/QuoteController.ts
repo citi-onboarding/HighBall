3c357c1bdeebfe581f35d22309fab579a3fcba5e
virolivs <103077000+virolivs@users.noreply.github.com>
2022-05-25 23:36:09 -0300

    Update QuoteController.ts

--- server/src/controllers/QuoteController.ts
+++ server/src/controllers/QuoteController.ts
@@ -5,9 +5,9 @@ import { Citi, Crud } from '../global'
 export default class QuoteController implements Crud {
 
     async create(request: Request, response: Response){
-        const {quote, description, link} = request.body;
+        const {sentence, description, link} = request.body;
 
-        const isAnyUndefined = Citi.areValuesUndefined(quote, description, link);
+        const isAnyUndefined = Citi.areValuesUndefined(sentence, description, link);
         if(isAnyUndefined) return response.status(400).send();
 
         const newUser = { quote, description, link };
@@ -16,4 +16,35 @@ export default class QuoteController implements Crud {
         return response.status(httpStatus).send({ message });
     }
 
+  
+    async get(request: Request, response: Response){
+        const {httpStatus, values} = await Citi.getAll(quote);
+        return response.status(httpStatus).send(values);
+    }
+
+    async delete(request: Request, response: Response){
+        const { id } = request.params;
+        const {value: quoteFound, message } = await Citi.findByID(quote, id); 
+           
+        if(!quoteFound) return response.status(400).send({ message });
+
+        const {httpStatus, messageFromDelete } = await Citi.deleteValue(quote, quoteFound);
+        return response.status(httpStatus).send({ messageFromDelete });
+    }
+
+    async update(request: Request, response: Response){
+        const { id } = request.params;
+        const { sentence, description, link } = request.body;
+
+        const isAnyUndefined = Citi.areValuesUndefined(sentence, description, link, id);
+        if(isAnyUndefined) return response.status(400).send();
+
+        const userWithUpdatedValues = { sentence, description, link };
+
+        const { httpStatus, messageFromUpdate } = await Citi.updateValue(quote, id, userWithUpdatedValues);
+        return response.status(httpStatus).send({ messageFromUpdate });
+    }
+
+    
+}
   
