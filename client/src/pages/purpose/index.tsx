0190aba2173c2c8efcbb7d6dfe482b99e0edc76b
Monterazo <lsm6@cin.ufpe.br>
2022-06-01 15:10:50 -0300

    feature(purpose): component structure

--- client/src/pages/purpose/index.tsx
+++ client/src/pages/purpose/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import {
   Container,
+  SideImages,
+  Content,
+  Title,
+  Description,
 } from './styles';
 
 
 export function Purpose(){
   return(
     <Container>
-
+      <SideImages/>
+      <Content>
+        <Title>
+          Propósito
+        </Title>
+        <Description>
+        Criada em 2021 por duas amigas com o intuito de dinamizar a arte de consumir bebidas, implementando e promovendo um design sofisticado e personalizado a cada gole, através de copos desenhados por especialistas.<br/><br/>Possuimos um portfólio amplo e em crescente modificação e estamos sempre disponiveis para sentar com os nossos clientes e desenhar a melhor solucao que se adeque a sua realidade e necessidade.
+        </Description>
+  
+      </Content>
     </Container>
   );
 
--- client/src/pages/purpose/styles.ts
+++ client/src/pages/purpose/styles.ts
@@ -11,3 +11,30 @@ export const Container = styled.section`
   border-radius: 5px;
   color: white;
 `;
+
+export const SideImages = styled.aside`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 613px;
+  width: 580px;
+  border-radius: 5px;
+  color: white;
+`;
+
+
+export const Content = styled.div `
+`
+
+export const Title = styled.h2`
+  font-size: 42px;
+  color: #7B54C9;
+ 
+`;
+
+export const Description = styled.p `
+  font-size: 18px;
+  line-height: 22px;
+`
+
+
